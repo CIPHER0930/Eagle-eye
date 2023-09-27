791913201b512ab83e4e77acf607cfc04de82daf
SHIELD660 <richmondrichmond183@gmail.com>
2023-09-27 15:12:13 +0100

    Update Eagle-eye.py

--- Eagle-eye.py
+++ Eagle-eye.py
@@ -6,74 +6,91 @@ import subprocess
 import transformers
 import pyttsx3
 import speech_recognition
+import mediapipe as mp
+import cv2
+import numpy as np
 
 class MultilingualChatbot:
- def __init__(self, languages):
-  self.languages = languages
-  self.tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
-  self.model = AutoModelForSeq2SeqLM.from_pretrained("google/mt5-small")
-  self.recognizer = speech_recognition.Recognizer()
-  self.engine = pyttsx3.init()
-  self.engine.setProperty("voice", "en+f1")
+ def __init__(self, languages):
+ self.languages = languages
+ self.tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
+ self.model = AutoModelForSeq2SeqLM.from_pretrained("google/mt5-small")
+ self.recognizer = speech_recognition.Recognizer()
+ self.engine = pyttsx3.init()
+ self.engine.setProperty("voice", "en+f1")
 
- def detect_language(self, text):
-  """Detects the language of a given text."""
+ def detect_language(self, text):
+ """Detects the language of a given text."""
 
-  url = "https://translation.googleapis.com/translate_v2/detect"
-  params = {"q": text, "key": "YOUR_API_KEY"}
-  response = requests.get(url, params=params)
-  data = json.loads(response.content)
-  return data["detections"][0]["language"]
+ url = "https://translation.googleapis.com/translate_v2/detect"
+ params = {"q": text, "key": "YOUR_API_KEY"}
+ response = requests.get(url, params=params)
+ data = json.loads(response.content)
+ return data["detections"][0]["language"]
 
- def translate(self, text, source_language, target_language):
-  """Translates a given text from one language to another."""
+ def translate(self, text, source_language, target_language):
+ """Translates a given text from one language to another."""
 
-  url = "https://translation.googleapis.com/translate_v2/translate"
-  params = {
-   "q": text,
-   "source": source_language,
-   "target": target_language,
-   "key": "YOUR_API_KEY",
-  }
-  response = requests.post(url, params=params)
-  data = json.loads(response.content)
-  return data["translations"][0]["translatedText"]
+ url = "https://translation.googleapis.com/translate_v2/translate"
+ params = {
+ "q": text,
+ "source": source_language,
+ "target": target_language,
+ "key": "YOUR_API_KEY",
+ }
+ response = requests.post(url, params=params)
+ data = json.loads(response.content)
+ return data["translations"][0]["translatedText"]
 
- def respond(self, text, user_language):
-  """Generates a response to a given text in the specified language."""
+ def respond(self, text, user_language):
+ """Generates a response to a given text in the specified language."""
 
-  try:
-    # Detect the type of pentesting task that the user is requesting.
-    pentesting_task = self.detect_pentesting_task(text)
+ try:
+ # Detect the type of pentesting task that the user is requesting.
+ pentesting_task = self.detect_pentesting_task(text)
 
-    # If the user is requesting a pentesting task, call the appropriate script.
-    if pentesting_task is not None:
-      # Detect the pentesting phase that the user is requesting.
-      pentesting_phase = self.detect_pentesting_phase(text)
+ # If the user is requesting a pentesting task, call the appropriate script.
+ if pentesting_task is not None:
+ # Detect the pentesting phase that the user is requesting.
+ pentesting_phase = self.detect_pentesting_phase(text)
 
-      # Call the appropriate script to perform the pentesting phase.
-      pentesting_phase_results = self.run_pentesting_phase_script(pentesting_phase)
+ # Call the appropriate script to perform the pentesting phase.
+ pentesting_phase_results = self.run_pentesting_phase_script(pentesting_phase)
 
-      # Generate a report on the results of the pentesting phase.
-      pentesting_phase_report = self.generate_pentesting_phase_report(pentesting_phase_results)
+ # Generate a report on the results of the pentesting phase.
+ pentesting_phase_report = self.generate_pentesting_phase_report(pentesting_phase_results)
 
-      # Return the pentesting phase report to the user.
-      return pentesting_phase_report
+ # Return the pentesting phase report to the user.
+ return pentesting_phase_report
 
-    # If the user is not requesting a pentesting task, call the original `respond()` method.
-    else:
-      response = super().respond(text, user_language)
+ # If the user is not requesting a pentesting task, call the original `respond()` method.
+ else:
+ response = super().respond(text, user_language)
 
-      # If the user wants a voice response, speak it.
-      if self.user_wants_voice_response(text):
-        self.speak(response, user_language)
+ # If the user wants a voice response, speak it.
+ if self.user_wants_voice_response(text):
+ self.speak(response, user_language)
 
-  except Exception as e:
-    # Log the error and return a friendly message to the user.
-    logging.error(e)
-    return "Sorry, I encountered an error while processing your request. Please try again later."
+ # If the user wants to perform object recognition, call the `recognize_objects()` function.
+ if "recognize objects" in text:
+ image = self.get_image_from_camera()
+ detections = self.recognize_objects(image)
+ response = f"I found the following objects: {detections}"
 
-  finally:
-    # Clean up any resources that may have been opened.
-    pass
+ # If the user wants to perform object detection, call the `detect_objects()` function.
+ elif "detect objects" in text:
+ image = self.get_image_from_camera()
+ bounding_boxes = self.detect_objects(image)
+ response = f"I found the following bounding boxes: {bounding_boxes}"
 
+ # If the user wants to perform car tracking, call the `track_cars()` function.
+ elif "track cars" in text:
+ image = self.get_image_from_camera()
+ bounding_boxes = self.track_cars(image)
+ response = f"I found the following tracked cars: {bounding_boxes}"
+
+ # If the user wants to get the GPS coordinates of an object, call the `get_gps_coordinates()` function.
+ elif "get GPS coordinates" in text:
+ bounding_box = self.get_bounding_box_from_user(text)
+ gps_coordinates = self.get_gps_coordinates(bounding_box)
+ response = f"The GPS coordinates of the object are: {gps
