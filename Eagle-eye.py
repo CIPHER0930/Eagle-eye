c911dda082c78f855061bd298001b27a386ddcb8
SHIELD660 <richmondrichmond183@gmail.com>
2023-09-29 02:38:22 +0100

    Update Eagle-eye.py

--- Eagle-eye.py
+++ Eagle-eye.py
@@ -11,7 +11,7 @@ import cv2
 import numpy as np
 
 # Display the image
-image = cv2.imread("eagle_eye.png")
+image = cv2.imread("images (2).jpeg")
 cv2.imshow("Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
